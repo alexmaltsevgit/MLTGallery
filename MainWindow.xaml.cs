c8e6a2f949fcb37792d394f57e217821825815bf
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-06-01 00:23:20 +0300

    project skelet built

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Forms;
-using System.Threading;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Input;
 
 namespace MLTGallery
 {
@@ -25,11 +18,13 @@ namespace MLTGallery
     private List<Image> images = new List<Image>();
     private double imgWidth;
     private readonly Thickness imgMargin = new Thickness(20);
+    private readonly string[] extensions = { "jpg", "jpeg", "jpe", "png", "bmp" };
     private bool isLoading = false;
 
     public MainWindow()
     {
       InitializeComponent();
+      PreviewMouseWheel += Window_PreviewMouseWheel;
     }
 
     private void OnLoad(object sender, RoutedEventArgs e)
@@ -37,15 +32,48 @@ namespace MLTGallery
       SetItemWidth(300);
     }
 
-    private BitmapImage GetBitmapImage()
+    private void OpenDirectory(object sender, RoutedEventArgs e)
     {
-      OpenFileDialog fileDialog = new OpenFileDialog();
-      fileDialog.ShowDialog();
+      FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+      var result = folderDialog.ShowDialog();
+      if (result == System.Windows.Forms.DialogResult.OK)
+      {
+        AddAllImages(folderDialog.SelectedPath);
+      }
+    }
 
-      Uri uri = new Uri(fileDialog.FileName);
+    private void AddAllImages(string rootPath)
+    {
+      DirectoryInfo dir = new DirectoryInfo(rootPath);
+
+      DirectoryInfo[] subdirs = dir.GetDirectories();
+      foreach (var subdir in subdirs)
+      {
+        AddAllImages(subdir.FullName);
+      }
+      
+      string[] files = Directory.GetFiles(rootPath);
+      foreach (var file in files)
+      {
+        //if (extensions.Contains(file.Extension))
+        //{
+          AddImage(file);
+        //}
+      }
+    }
+
+    private void AddImage(string path)
+    {
+      Uri uri = new Uri(path);
       BitmapImage src = new BitmapImage(uri);
+      Image img = new Image
+      {
+        Source = src,
+        Margin = imgMargin
+      };
 
-      return src;
+      images.Add(img);
+      wpImages.Children.Add(img);
     }
 
     private void AddImage(object sender, RoutedEventArgs e)
@@ -66,10 +94,43 @@ namespace MLTGallery
       wpImages.Children.Add(img);
     }
 
+    private BitmapImage GetBitmapImage()
+    {
+      OpenFileDialog fileDialog = new OpenFileDialog();
+      fileDialog.ShowDialog();
+
+      Uri uri = new Uri(fileDialog.FileName);
+      BitmapImage src = new BitmapImage(uri);
+
+      return src;
+    }
+
     private void SetItemWidth(double value)
     {
       imgWidth = value;
       wpImages.ItemWidth = value;
+    }
+
+    private void Window_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      if (Keyboard.Modifiers != ModifierKeys.Control)
+        return;
+
+      if (e.Delta > 0)
+        ZoomIn();
+
+      else if (e.Delta < 0)
+        ZoomOut();
+    }
+
+    private void ZoomIn()
+    {
+      if (imgWidth < Window.GetWindow(window).ActualWidth) { SetItemWidth(imgWidth * 1.5); }
+    }
+
+    private void ZoomOut()
+    {
+      if (imgWidth > 100) { SetItemWidth(imgWidth / 1.5); }
     }
   }
 };
